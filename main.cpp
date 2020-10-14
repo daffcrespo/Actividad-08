974bbbdda2ab4d6f9a46eb2dcbbf7320d5cb13b2
Dafne Crespo <dafne.crespo4000@alumnos.udg.mx>
2020-10-13 20:19:25 -0500

    modificamos todo el programa para implementar la sobrecarga de operadores

--- main.cpp
+++ main.cpp
@@ -9,15 +9,20 @@ main(){
     Computadora pc2 = Computadora("Jaqueline Romo", "Linux", 12, 128);
     Computadora pc3 = Computadora("Sam Glez", "Windows", 12, 256);
     Computadora pc4 = Computadora("Esperancita", "iOS", 12, 256);
-    Computadora pc5 = Computadora("Carlos H", "Linux", 8, 128);
+    //Computadora pc5 = Computadora("Carlos H", "Linux", 8, 128);
 
     Laboratorio lc;
 
-    lc.agregarComputadora(pc1);
+    lc << pc1 << pc2 << pc3 << pc4;
+    Computadora pc5;
+    cin >> pc5;
+    lc.agregarComputadora(pc5);
+
+    /*lc.agregarComputadora(pc1);
     lc.agregarComputadora(pc2);
     lc.agregarComputadora(pc3);
     lc.agregarComputadora(pc4);
-    lc.agregarComputadora(pc5);
+    lc.agregarComputadora(pc5);*/
     lc.mostrar();
 
     /*pc1.setCapacidad(33);
